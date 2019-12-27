423862d704c127bd40df6bf1dffb94ae3bb70377
Erik Joling <erik@ejoweb.nl>
2019-12-27 14:20:12 +0100

    Add check to determine if we should load the code

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,6 +5,11 @@
 
 namespace Ejo\Templating;
 
+// Do not load if it's already loaded
+if ( defined('EJO_TEMPLATING_IS_LOADED') ) {
+	return;
+}
+
 require_once __DIR__ . '/functions-debugging.php';
 require_once __DIR__ . '/functions-helpers.php';
 require_once __DIR__ . '/functions-filters.php';
@@ -15,3 +20,6 @@ require_once __DIR__ . '/components-setup/init.php';
 require_once __DIR__ . '/functions-templating-helpers.php';
 require_once __DIR__ . '/functions-templating-conditionals.php';
 require_once __DIR__ . '/functions-templating.php';
+
+// Set check
+define('EJO_TEMPLATING_IS_LOADED', true);
